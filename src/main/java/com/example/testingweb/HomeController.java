f37d8c084847a4318da86d57ee9e1c03ef63d167
hugogonzar <108681347+hugogonzar@users.noreply.github.com>
2022-09-12 16:11:48 -0300

    Update HomeController.java

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		return "Hello, World";
+		return "Finaliza el curso de DevOps Fundation - Hugo González Rosas";
 	}
 
 }
