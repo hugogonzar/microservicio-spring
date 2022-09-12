c6611ff250ea53473eccba9fccf73cc401612c7f
hugogonzar <108681347+hugogonzar@users.noreply.github.com>
2022-09-12 16:09:04 -0300

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
-		return "Hello, World";
+		return "Finaliza el curso de DevOps Fundation v2 - Hugo González Rosas";
 	}
 }
